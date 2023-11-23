8635eda59dc9f0e3942fdeee396365e782c0d466
xungao <xungao@xungao.com>
2023-11-23 14:52:29 +0800

    Fix top Info

--- www/output_topinfo.sh
+++ www/output_topinfo.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-top_count=0
-echo "top_output 11111"
-while true; do 
-    if [ $top_count -gt 9 ]; then                         
-        top -b -n 1 -d 0 | head -2 > /data/svm/top_output.txt
-        echo "top_output"
-        top_count=0
-    fi                                                   
-    top_count=`expr $top_count + 1`
-    sleep 1                                                          
-    echo "top_output 2222"
-done 
-echo "top_output 333"
-echo "top_output 333"
-echo "top_output 333"
-echo "top_output 333"
+top -b -n 1 -d 0 | head -2 > /data/svm/top_output.txt
+cat /data/svm/top_output.txt | head -1 > /data/svm/board_meminfo.txt
+cat /data/svm/top_output.txt | head -2 | tail -1 > /data/svm/board_cpuloadinfo.txt
 
 
 
